c28f77f588bb8cc70ccfcfcfed92355a52a6f565
soneunsu <kekekee257@gmail.com>
2025-01-10 14:29:35 +0900

    [minsu-001] 칭호시스템 구현 못하고 레벨만 가져옴

--- TextRPG/TextRPG/02_manager/player_manager.cpp
+++ TextRPG/TextRPG/02_manager/player_manager.cpp
@@ -1,10 +1,16 @@
 #include "player_manager.h"
 #include "../03_ingame/player/player.h"
 #include <iostream>
+#include <string>
 using namespace std;
 Player PlayerManager::CreatePlayer()
 {
 	Player player;
 	player.SetInfo(200);
 	return player;
+}
+
+int PlayerManager::GetLevel()
+{
+	return level;
 }
--- TextRPG/TextRPG/TextRPG.cpp
+++ TextRPG/TextRPG/TextRPG.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "02_manager/player_manager.h"
 #include "02_manager/game_manager.h"
+
 int main()
 {
     GameManager* gameManager = GameManager::GetInstance();
