cd39d2f5e5913ac1dc3ca2cda0e08d94289963e4
Min <kansin4@naver.com>
2025-01-10 15:06:18 +0900

    Update game_manager.cpp

--- TextRPG/TextRPG/02_manager/game_manager.cpp
+++ TextRPG/TextRPG/02_manager/game_manager.cpp
@@ -45,9 +45,7 @@ void GameManager::CreatePlayer()
 	PlayerManager* playerManager = PlayerManager::GetInstance();
 	Player player = playerManager->CreatePlayer();
 	cout << "생성된 플레이어 체력: " << player.GetHp() << " 레벨: " << playerManager->GetLevel() << endl;
-	cout << "캬 버그좀 느껴봐라~~" << endl;
 
-	cout << "가윤) 민수 요즘 맘에 안들어" << endl;
 }
 
 void GameManager::Battle()
