ebc77669d02c03f4355d30e2807074251ef0ad85
soneunsu <kekekee257@gmail.com>
2025-01-13 17:04:44 +0900

    [eunsu-002] GUI 클래스 추가

---
+++ TextRPG/TextRPG/04_Util/gui.h
@@ -0,0 +1,51 @@
+#include <windows.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+static class GUI
+{
+public:
+	static void GoToXY(int x, int y)
+	{
+		COORD coord = { x, y };
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+	}
+
+	static void DrawBox(int x, int y, int width, int height)
+	{
+		for (int i = 0; i < height; i++)
+		{
+			GoToXY(x, y + i);
+			if (i == 0 || i == height - 1) {
+				cout << "+" << string(width - 2, '-') << "+";
+			}
+			else {
+				cout << "|" << string(width - 2, ' ') << "|";
+			}
+		}
+	}
+
+	static void PrintTitleText() {
+		string text[] = {
+			" _____ __  __  ___      _ ___           ____  ____   ____ ",
+			"| ____|  \\/  |/ _ \\    | |_ _|         |  _ \\|  _ \\ / ___|",
+			"|  _| | |\\/| | | | |_  | || |   _____  | |_) | |_) | |  _ ",
+			"| |___| |  | | |_| | |_| || |  |_____| |  _ <|  __/| |_| |",
+			"|_____|_|  |_|\\___/ \\___/|___|         |_| \\_\\_|    \\____|",
+		};
+		GoToXY(31, 4);
+		for (int i = 0; i < 29; i++) cout << "\xF0\x9F\x98\x81";
+		GoToXY(31, 5);
+		cout << text[0] << endl;
+		GoToXY(31, 6);
+		cout << text[1] << endl;
+		GoToXY(31, 7);
+		cout << text[2] << endl;
+		GoToXY(31, 8);
+		cout << text[3] << endl;
+		GoToXY(31, 9);
+		cout << text[4] << endl;
+		GoToXY(31, 10);
+		for (int i = 0; i < 29; i++) cout << "\xF0\x9F\x98\x81";
+	}
+};
